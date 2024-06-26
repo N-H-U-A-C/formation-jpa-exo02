f60f96b6b818feada84d0462a1d6685ecef5fc52
cedric <cedric.belot.dev@gmail.com>
2024-06-26 15:42:23 +0200

    feat: create Client class

---
+++ src/main/java/dev/cb/boxoffice/model/Client.java
@@ -0,0 +1,49 @@
+package dev.cb.boxoffice.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "client_id")
+    private int id;
+    @Column(name = "client_firstname")
+    private String firstName;
+    @Column(name = "client_lastname")
+    private String lastName;
+    @Column(name = "client_birthdate")
+    private LocalDate birthDate;
+    @Column(name = "client_phonenumbers")
+    private String phoneNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "adress_id")
+    private Address address;
+
+    public Client() {
+    }
+
+    public Client(String firstName, String lastName, LocalDate birthDate, String phoneNumber, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate=" + birthDate +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
